b0919e654002bc7326f83cd6cbcfb5aa22b129a4
Cristiano Borges de Morais <cristianoborgesntc@gmail.com>
2024-06-29 15:33:36 -0300

    [Feat] Adicionando página de tarefas.

---
+++ src/pages/Tasks.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ActivitiesComponent from "../components/ActivitiesComponent";
+
+
+export default function Activities() {
+  return(<div> <ActivitiesComponent/> </div>)    
+}
